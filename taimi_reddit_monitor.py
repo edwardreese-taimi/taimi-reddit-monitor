d2ac384ef3da80febd27040534e91d34a8a552d1
edwardreese-taimi <edward.reese@appflame.com>
2026-08-14 13:40:33 +0300

    Add r/transplace to COMMENT_SUBREDDITS

--- taimi_reddit_monitor.py
+++ taimi_reddit_monitor.py
@@ -31,7 +31,7 @@ COMMENT_SUBREDDITS = [
     "dating_advice", "OnlineDating", "datingapps", "Tinder",
     "relationships", "relationship_advice", "relationships_advice",
     "Sissy", "PNW_Sissies", "sissyology", "bisexualafterdark",
-    "countttt", "BDSMsapphic", "phallo", "BlackTransPeople",
+    "countttt", "BDSMsapphic", "phallo", "BlackTransPeople", "transplace",
 ]
 
 POSITIVE_WORDS = {
