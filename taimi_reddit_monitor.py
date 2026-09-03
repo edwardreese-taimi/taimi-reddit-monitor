c1e6383f766efe1974355baf64d3fddd2b3dbc3a
edwardreese-taimi <edward.reese@appflame.com>
2026-09-03 15:30:03 +0300

    Update taimi_reddit_monitor.py

--- taimi_reddit_monitor.py
+++ taimi_reddit_monitor.py
@@ -58,6 +58,7 @@ NEGATIVE_WORDS = {
     "leaving", "leave", "switching", "switch", "confused", "alternative", "alternatives",
     "non-functional", "annoying", "mess", "fetishist", "fetishists",
     "awkward", "confusion", "confusing", "spam", "spamming", "unauthorized", "unwanted", "apologize", "apologizing",
+    "ai",
 }
 
 MIXED_WORDS = {"mixed", "mixed reviews", "unsure", "uncertain", "depends", "varies"}
